1ad916c0da96b72b9b9eb0c19a7d5c84164c875b
LeoLivs <63762816+LeoLivs@users.noreply.github.com>
2020-11-25 21:32:01 -0500

    second attempt
    first attempt deleted itself somehow

--- python-stuff/main.py
+++ python-stuff/main.py
@@ -16,3 +16,28 @@ def product(numA, numB):
   return theProduct
 
 print( "The product of " + str(a) + " and " + str(b) + " is -> " + str(product(b, a)))
+
+# create a function that subtracts two numbers and returns the difference
+def difference(num1, num2):
+    theDifference = num1 - num2
+    return theDifference
+
+
+print(str(difference(c, b) + "is the difference between" + float(c) - float(b)))
+
+# create a function that divides two numbers and returns the dividend? Hope thats right lol
+
+def dividend(numX, numT):
+  theDividend = numX / numT
+  return theDividend
+
+
+print(dividend(b,a))
+
+# create a function that determines the percentage of 2 numbers
+
+def prcnt(num1, num2):
+  thePercentage = num1 % num2
+  return thePercentage
+
+print(prcnt(b,c))
